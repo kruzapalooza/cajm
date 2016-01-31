2351cc0defd02ffcc0c00dd29984236ae1fb32e4
kruzapalooza <mkruza@gmail.com>
2016-01-31 08:44:45 -0500

    troubleshooting yml looping in faqs.js

--- source/assets/javascripts/faqs.js
+++ source/assets/javascripts/faqs.js
@@ -2,18 +2,51 @@ var $yaml_file = "/data/faqs.yml";
 
 jQuery(document).ready( function($){
 
-  $.ajax({ url: $yaml_file, success: function(file_content) {
+  $.ajax({ url: $yaml_file, success: function (file_content) {
 
-    test_string = YAML.eval(file_content);
+    var level_zero = YAML.eval(file_content);
 
-    test_string.faqs.forEach(function(elem) {
-        console.log(elem);
+      console.log(level_zero);
+      console.log(typeof(level_zero));
+      console.log(level_zero.length);
+      console.log('\n');
+      console.log(level_zero.faqs);
 
-        for (var prop in elem) {
-          console.log("elem." + prop + " = " + elem[prop]);
+      for (i=0 ; i < level_zero.faqs.length ; i++) {
+        console.log(i);
+        console.log(level_zero.faqs[i]);
+        console.log('\n');
+
+        for (var level_two in level_one) {
+          console.log(level_two);
+          console.log(typeof(level_two));
+          console.log(level_one[level_two]);
+          console.log('\n');
         }
 
-    });
+      }
+
+      for (var level_one in level_zero.faqs[i]) {
+        console.log(level_one);
+        console.log(typeof(level_one));
+        console.log(level_one);
+        console.log('\n');
+
+        for (j=0 ; j < level_one.length ; j++) {
+          console.log(j);
+          console.log(level_one[j]);
+          console.log('\n');
+        }
+
+        for (var level_two in level_one) {
+          console.log(level_two);
+          console.log(typeof(level_two));
+          console.log(level_one[level_two]);
+          console.log('\n');
+        }
+
+      }
+
 
     }
 
