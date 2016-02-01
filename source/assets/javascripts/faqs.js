7418c43c04af4d7cac931ffbdeeff31c97620c21
kruzapalooza <mkruza@gmail.com>
2016-01-31 20:31:07 -0500

    solved nesting and looping issues from yaml input

--- source/assets/javascripts/faqs.js
+++ source/assets/javascripts/faqs.js
@@ -6,46 +6,79 @@ jQuery(document).ready( function($){
 
     var level_zero = YAML.eval(file_content);
 
-      console.log(level_zero);
-      console.log(typeof(level_zero));
-      console.log(level_zero.length);
-      console.log('\n');
-      console.log(level_zero.faqs);
+    // console.log(level_zero.faqs[0].q_a.question);
+    // console.log(level_zero.faqs[0].q_a.answer);
+    // console.log(level_zero.faqs[0].q_a.answer_ol[0]);
+    // console.log(level_zero.faqs[0].q_a.answer_ol[1]);
+    // console.log(level_zero.faqs[1].q_a.question);
+    // console.log(level_zero.faqs[1].q_a.answer);
+    // console.log(level_zero.faqs[1].q_a.answer_p);
 
-      for (i=0 ; i < level_zero.faqs.length ; i++) {
-        console.log(i);
-        console.log(level_zero.faqs[i]);
-        console.log('\n');
+    console.log('\n');
 
-        for (var level_two in level_one) {
-          console.log(level_two);
-          console.log(typeof(level_two));
-          console.log(level_one[level_two]);
-          console.log('\n');
+    for (var level_one in level_zero.faqs) {
+
+
+      $content = "";
+      for (var level_two in level_zero.faqs[level_one]) {
+
+        if (level_two === 'q_a') {
+
+          // $('#nehemiah-turnout').each( function (){
+          //     var content = '<div class=\"row\">';
+          //     nehemiah_turnout.forEach( function(i){
+          //       content += '<div class=\"cell\">' + i +'</div>' ;
+          //       console.log(i); 
+          //   });
+          //     content += "</div>"
+          //   $(this).html( content ); 
+          // });
+
+          $('#thumbnail-wrap-faqs').each( function (){
+            $("thumbnail-wrap-faqs").append("<div class=\"thumbnail\"><div>")
+          });
+
+          // thumbnail-wrap-faqs
+
+          //content += 
+
+
+
+          for (var level_three in level_zero.faqs[level_one][level_two]) {
+            console.log('l3 key: ' + level_three);
+            console.log(level_zero.faqs[level_one][level_two][level_three]);
+            console.log('\n');
+
+            if (level_three === 'question') {
+              console.log('Q');
+            }
+
+            if (level_three === 'answer') {
+              console.log('A');
+            }
+
+            if (level_three === 'answer_p') {
+              console.log('P');
+            }        
+
+            if (level_three === 'answer_ol') {
+              console.log('OL');
+            } 
+
+            if (level_three === 'answer_table') {
+              console.log('T');
+            } 
+
+            if (level_three === 'two_column_list') {
+              console.log('2CL');
+            } 
+          } 
         }
-
       }
 
-      for (var level_one in level_zero.faqs[i]) {
-        console.log(level_one);
-        console.log(typeof(level_one));
-        console.log(level_one);
-        console.log('\n');
 
-        for (j=0 ; j < level_one.length ; j++) {
-          console.log(j);
-          console.log(level_one[j]);
-          console.log('\n');
-        }
 
-        for (var level_two in level_one) {
-          console.log(level_two);
-          console.log(typeof(level_two));
-          console.log(level_one[level_two]);
-          console.log('\n');
-        }
-
-      }
+    }
 
 
     }
