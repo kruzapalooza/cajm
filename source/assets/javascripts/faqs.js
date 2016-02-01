96f02197e43e72fb760add50c28aa787831a85f3
kruzapalooza <mkruza@gmail.com>
2016-02-01 05:20:30 -0500

    starting adding html faqs thumbnails via js

--- source/assets/javascripts/faqs.js
+++ source/assets/javascripts/faqs.js
@@ -3,46 +3,16 @@ var $yaml_file = "/data/faqs.yml";
 jQuery(document).ready( function($){
 
   $.ajax({ url: $yaml_file, success: function (file_content) {
-
     var level_zero = YAML.eval(file_content);
-
-    // console.log(level_zero.faqs[0].q_a.question);
-    // console.log(level_zero.faqs[0].q_a.answer);
-    // console.log(level_zero.faqs[0].q_a.answer_ol[0]);
-    // console.log(level_zero.faqs[0].q_a.answer_ol[1]);
-    // console.log(level_zero.faqs[1].q_a.question);
-    // console.log(level_zero.faqs[1].q_a.answer);
-    // console.log(level_zero.faqs[1].q_a.answer_p);
-
     console.log('\n');
-
     for (var level_one in level_zero.faqs) {
-
 
       $content = "";
       for (var level_two in level_zero.faqs[level_one]) {
 
         if (level_two === 'q_a') {
 
-          // $('#nehemiah-turnout').each( function (){
-          //     var content = '<div class=\"row\">';
-          //     nehemiah_turnout.forEach( function(i){
-          //       content += '<div class=\"cell\">' + i +'</div>' ;
-          //       console.log(i); 
-          //   });
-          //     content += "</div>"
-          //   $(this).html( content ); 
-          // });
-
-          $('#thumbnail-wrap-faqs').each( function (){
-            $("thumbnail-wrap-faqs").append("<div class=\"thumbnail\"><div>")
-          });
-
-          // thumbnail-wrap-faqs
-
-          //content += 
-
-
+          $content += "<div class=\"thumbnail\"> TEST" ;
 
           for (var level_three in level_zero.faqs[level_one][level_two]) {
             console.log('l3 key: ' + level_three);
@@ -73,7 +43,14 @@ jQuery(document).ready( function($){
               console.log('2CL');
             } 
           } 
+
+          $content += "<div>" ;
+
         }
+
+        $('#thumbnail-wrap-faqs').each( function (){
+          $("#thumbnail-wrap-faqs").append($content);
+        }); 
       }
 
 
