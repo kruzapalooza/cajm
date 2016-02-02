e81f9d6c8583b0ea5ebab5efb3a40d6d30a23db6
kruzapalooza <mkruza@gmail.com>
2016-02-01 19:58:49 -0500

    tried adding top-wrap, will try again tomorrow

--- source/assets/javascripts/faqs.js
+++ source/assets/javascripts/faqs.js
@@ -7,24 +7,36 @@ jQuery(document).ready( function($){
     console.log('\n');
     for (var level_one in level_zero.faqs) {
 
-      $content = "";
+      $thumbnail_content = "";
+      $top_wrap_content  = "";
       for (var level_two in level_zero.faqs[level_one]) {
 
         if (level_two === 'q_a') {
 
-          $content += "<div class=\"thumbnail\"> TEST" ;
+          $thumbnail_content += "<div class=\"thumbnail\">" ;
+          // $top_wrap_content  += "<h2>" ;
 
           for (var level_three in level_zero.faqs[level_one][level_two]) {
             console.log('l3 key: ' + level_three);
             console.log(level_zero.faqs[level_one][level_two][level_three]);
             console.log('\n');
 
-            if (level_three === 'question') {
-              console.log('Q');
+            if (level_three === 'question') { 
+
+              $thumbnail_content += "<div class=\"thumbnail-title\">";
+              $thumbnail_content += level_zero.faqs[level_one][level_two][level_three];
+              $thumbnail_content += "</div>";
+
+              // $top_wrap_content  += level_zero.faqs[level_one][level_two][level_three];
             }
 
             if (level_three === 'answer') {
-              console.log('A');
+
+              $thumbnail_content += "<div class=\"thumbnail-descr\">";
+              $thumbnail_content += level_zero.faqs[level_one][level_two][level_three];
+              $thumbnail_content += "</div>";
+
+              // $top_wrap_content  += level_zero.faqs[level_one][level_two][level_three];            
             }
 
             if (level_three === 'answer_p') {
@@ -44,13 +56,19 @@ jQuery(document).ready( function($){
             } 
           } 
 
-          $content += "<div>" ;
+          $thumbnail_content += "</div>" ;
+          // $top_wrap_content  += "</h2>" ;
 
         }
 
         $('#thumbnail-wrap-faqs').each( function (){
-          $("#thumbnail-wrap-faqs").append($content);
+          $("#thumbnail-wrap-faqs").append($thumbnail_content);
         }); 
+
+        $('#top-wrap-faqs').each( function (){
+          $("#top-wrap-faqs").append($top_wrap_content);
+        });
+
       }
 
 
