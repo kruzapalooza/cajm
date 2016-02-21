b9e96bb867f3efd51bb63ca7ac42b26ee98e9cae
kruzapalooza <mkruza@gmail.com>
2016-02-21 13:19:46 -0500

    finished faqs, added dynamic links to thumbnails

--- source/assets/javascripts/faqs.js
+++ source/assets/javascripts/faqs.js
@@ -6,7 +6,7 @@ jQuery(document).ready( function($){
     var l0 = YAML.eval(file_content);
     console.log('\n');
 
-
+    $faq_number         = 0;
     for (var l1 in l0.faqs) {
 
       $thumbnail_content = "";
@@ -17,9 +17,11 @@ jQuery(document).ready( function($){
         // and it's text, tables and lists.
         if (l2 === 'q_a') {
 
+          $faq_number        += 1;
           $thumbnail_content += "<div class=\"thumbnail\">" ;
           $top_wrap_content  += "<div class=\"top-wrap\">";
 
+          // Make sure item starts with "q_a" (otherwise, do nothing)
           for (var l3 in l0.faqs[l1][l2]) {
             console.log('l3 key: ' + l3);
             console.log(l0.faqs[l1][l2][l3]);
@@ -32,6 +34,7 @@ jQuery(document).ready( function($){
               $thumbnail_content +=   l0.faqs[l1][l2][l3];
               $thumbnail_content += "</div>";
  
+              $top_wrap_content  += "<a class=\"anchor\" id=\"faq_number_" + $faq_number + "\"></a>";
               $top_wrap_content  += "<div class=\"top-wrap description\">";
               $top_wrap_content  +=   "<span class=\"dancing-cursive big-q\">";
               $top_wrap_content  +=     "Q:";
@@ -40,21 +43,24 @@ jQuery(document).ready( function($){
               $top_wrap_content  += "</div>";
             }
 
-            // A: The first phrase or sentence of an answer.
+            // A: The first phrase / headline of an answer, 
+            // this also links / jumps to the full faq article below.
             if (l3 === 'answer') {
 
-              $thumbnail_content += "<div class=\"thumbnail-descr\">";
-              $thumbnail_content += l0.faqs[l1][l2][l3];
-              $thumbnail_content += "</div>";
+              $thumbnail_content += "<div class=\"button-link\"> <ul> <li>" ;       
+              $thumbnail_content +=   "<a href=\"#faq_number_" + $faq_number + "\"> <span>";
+              $thumbnail_content +=      l0.faqs[l1][l2][l3]; 
+              $thumbnail_content +=   "</a> </span>" ;                                     
+              $thumbnail_content += "</li> </ul> </div>";
 
               $top_wrap_content  +=   "<span class=\"dancing-cursive big-a\">";
               $top_wrap_content  +=     "A: ";
               $top_wrap_content  +=   "</span>";
-              $top_wrap_content  += l0.faqs[l1][l2][l3];
-           
+              $top_wrap_content  += l0.faqs[l1][l2][l3];  
+
             }
 
-            // A: The rest of the answer text
+            // A: The rest of the answer text.
             if (l3 === 'answer_p') {
               $top_wrap_content  += "<p>";
               $top_wrap_content  +=   l0.faqs[l1][l2][l3];
@@ -144,12 +150,12 @@ jQuery(document).ready( function($){
                 if (which_side === "left")  { 
                   $top_wrap_content  += "<tr>";
                   $top_wrap_content  +=   "<td>";
-                  $top_wrap_content  +=  this_cell;
+                  $top_wrap_content  +=     this_cell;
                   $top_wrap_content  +=   "</td>";
                 }
                 else if (which_side === "right") { 
                   $top_wrap_content  +=   "<td>";                 
-                  $top_wrap_content  +=  this_cell;
+                  $top_wrap_content  +=     this_cell;
                   $top_wrap_content  +=   "</td>";
                   $top_wrap_content  += "</tr>";
                 }
@@ -171,20 +177,19 @@ jQuery(document).ready( function($){
 
         // write content stored to the html file that called this JQuery function.
         // This will be written to the thumbnails.
-        $('#thumbnail-wrap-faqs').each( function (){
+        $("#thumbnail-wrap-faqs").each( function (){
           $("#thumbnail-wrap-faqs").append($thumbnail_content);
         }); 
 
         // write content stored to the html file that called this JQuery function.
         // This will be written to the large wrap with the detailed answers, tables, lists.
-        $('#banner-faqs').each( function (){
+        $("#banner-faqs").each( function (){
           $("#banner-faqs").append($top_wrap_content);
         });
 
       }
 
     }
-
 
     }
 
