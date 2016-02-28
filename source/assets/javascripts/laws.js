93978e5a0a8e4ffacd7096794696827b206fbfa7
kruzapalooza <mkruza@gmail.com>
2016-02-27 20:13:22 -0500

    fixed laws.js

--- source/assets/javascripts/laws.js
+++ source/assets/javascripts/laws.js
@@ -11,10 +11,49 @@ jQuery(document).ready( function($){
 
     for (var l1 in l0.laws) {
 
-      console.log(l1);
-      console.log(l0.laws[l1].article);
+      $top_wrap_content  += "<div class=\"top-wrap\">" ;
+
+      if (l0.laws[l1].article.preamble) {
+
+        $top_wrap_content  += "<div class=\"dancing-cursive\">"
+        $top_wrap_content  +=     "<h2>" 
+        $top_wrap_content  +=        "Preamble"  
+        $top_wrap_content  +=    "</h2>"             
+        $top_wrap_content  += "</div>"
+        $top_wrap_content  += "<p>" ;
+        $top_wrap_content  +=   l0.laws[l1].article.preamble ;
+        $top_wrap_content  += "</p>" ;
+      }
+
+      if (l0.laws[l1].article.titl) {
+        $top_wrap_content  += "<p>" ;
+        $top_wrap_content  +=   l0.laws[l1].article.titl ;
+        $top_wrap_content  += "</p>" ;
+
+        $top_wrap_content  += "<div class=\"dancing-cursive\">"
+        $top_wrap_content  +=     "<h2> "
+        $top_wrap_content  +=         "Article I"  
+        $top_wrap_content  +=     "</h2>"   
+        $top_wrap_content  +=     "<h3>" 
+        $top_wrap_content  +=         "Incorporation"  
+        $top_wrap_content  +=     "</h3>"            
+        $top_wrap_content  += "</div>"
+      }
+
+      if (l0.laws[l1].article.section_ol) {
+        $top_wrap_content  +=  l0.laws[l1].article.section_ol ;
+      }
+
+      $top_wrap_content  += "</div>" ;
+
+      // write content stored to the html file that called this JQuery function.
+      $("#banner-laws").each( function (){
+        $("#banner-laws").append($top_wrap_content);
+      });
 
     }
+
+
 
   }
   });
